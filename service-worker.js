76808fdf997f781e86b65cc095269a9b026b2dc7
travismdgoldensports <travismd@gmail.com>
2025-10-20 16:03:59 -0600

    Commit

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "v1.0";
+const CACHE = "v1.2";
 const ASSETS = ["index.html", "manifest.webmanifest", "icons/icon-192.png", "icons/icon-512.png"];
 
 self.addEventListener("install", e => {
